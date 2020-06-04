c016b0b0b8f3addd8d9d99198ba5498eceae327c
samuelbunga <sambunga094@gmail.com>
2020-06-03 20:05:25 -0400

    modified 10x

--- pre_process/pre_process_10x.pl
+++ pre_process/pre_process_10x.pl
@@ -15,18 +15,18 @@ Contact:        bunga.s@husky.neu.edu
 
 
 GetOptions (	
-	    "sp=s" => \my $species, # M or H
-        "file=s" => \my $data, # path to the file
-        "out=s" => \my $name, # name of the project
-		"protocol=s" => \my $protocol, # protocol used
-		"sample_size=i" => \my $sample_size, # size of the sample
-		"qc=s" => \my $qc, # Perfrom QC switch(yes/no)
-		"aligner=s"	=> \my $aligner, # Choose between hisat2 and STAR(default is hisat2)
-		"email=s" => \my $to_email, # Email ID
-		"post_process=s" => \my $pp, # Post Process
-		"id=s" => \my $id, # Job name
-		"min_cells=i" => \my $min_cells, # min cells 
-		"min_genes=i" => \my $min_genes, # min genes
+	"sp=s" => \my $species, # M or H
+	"file=s" => \my $data, # path to the file
+	"out=s" => \my $name, # name of the project
+	"protocol=s" => \my $protocol, # protocol used
+	"sample_size=i" => \my $sample_size, # size of the sample
+	"qc=s" => \my $qc, # Perfrom QC switch(yes/no)
+	"aligner=s"	=> \my $aligner, # Choose between hisat2 and STAR(default is hisat2)
+	"email=s" => \my $to_email, # Email ID
+	"post_process=s" => \my $pp, # Post Process
+	"id=s" => \my $id, # Job name
+	"min_cells=i" => \my $min_cells, # min cells 
+	"min_genes=i" => \my $min_genes, # min genes
 	)
   or die("Error in command line arguments\n");
 
@@ -54,16 +54,16 @@ my $R2_path = join("/", $data, $R2);
 
 #Checking for the type of protocol
 if($protocol eq "celseq"){
-		$protocol        = "CCCCCCCCNNNNNN";
+	$protocol = "CCCCCCCCNNNNNN";
 }
 elsif($protocol eq "dropseq"){
-		$protocol        = "CCCCCCCCCCCCNNNNNNNN";
+	$protocol = "CCCCCCCCCCCCNNNNNNNN";
 }
 elsif($protocol eq "10Xv2"){
-		$protocol        = "CCCCCCCCCCCCCCCCNNNNNNNNNN";
+	$protocol = "CCCCCCCCCCCCCCCCNNNNNNNNNN";
 }
-    elsif($protocol eq "10Xv3"){
-           $protocol	 = "CCCCCCCCCCCCCCCCNNNNNNNNNNNN";
+elsif($protocol eq "10Xv3"){
+    $protocol = "CCCCCCCCCCCCCCCCNNNNNNNNNNNN";
 }
 
 chomp $protocol;
@@ -85,41 +85,27 @@ unless($qc){
 
 my($R2_name,$umi,$exe);
 $R2_name = "R2_extracted";
+
 if($qc eq "yes"){	
-say "running quality check using fastp";
-#Quality filter using FastP
-my $fastp = `/home/ubuntu/anaconda3/bin/fastp  -i $R1_path -I $R2_path -o $R1_out -O $R2_out`;
-say "done";
-
-say "Identify correct cell barcodes";
-$umi = `/home/ubuntu/anaconda3/bin/umi_tools whitelist --stdin $R1_out --bc-pattern=$protocol --set-cell-number=$sample_size --log2stderr > $wd/whitelist.txt`;
-say "done";
-
-say "Extract barcdoes and UMIs and add to read names";
-$umi = `/home/ubuntu/anaconda3/bin/umi_tools extract --bc-pattern=$protocol --stdin $R1_out --stdout $wd/R1_extracted.fastq.gz --read2-in $R2_out --read2-out=$wd/$R2_name.clean.fastq.gz --filter-cell-barcode --whitelist $wd/whitelist.txt`;   
-say "done";
-
-=cut
-say "Cleaning the fastq";
-$exe = `/usr/bin/perl /home/ubuntu/pre_process/clean.pl -file $wd/R2_extracted.fastq.gz -protocol $protocol_temp -out $wd/$R2_name.clean.fastq`;
-say "done";
-
-
-say "gunzip the clean fastq";
-my $gz = `/bin/gzip $wd/$R2_name.clean.fastq`;
-say "gunzip done";
-=cut
+    say "running quality check using fastp";
+    #Quality filter using FastP
+    my $fastp = `/home/ubuntu/anaconda3/bin/fastp  -i $R1_path -I $R2_path -o $R1_out -O $R2_out`;
+    say "done";
+    say "Identify correct cell barcodes";
+    $umi = `/home/ubuntu/anaconda3/bin/umi_tools whitelist --stdin $R1_out --bc-pattern=$protocol --set-cell-number=$sample_size --log2stderr > $wd/whitelist.txt`;
+    say "done";
+    say "Extract barcdoes and UMIs and add to read names";
+    $umi = `/home/ubuntu/anaconda3/bin/umi_tools extract --bc-pattern=$protocol --stdin $R1_out --stdout $wd/R1_extracted.fastq.gz --read2-in $R2_out --read2-out=$wd/$R2_name.clean.fastq.gz --filter-cell-barcode --whitelist $wd/whitelist.txt`;   
+    say "done";
 }
+
 elsif($qc eq "no"){
-
-say "Identify correct cell barcodes";
-$umi = `/home/ubuntu/anaconda3/bin/umi_tools whitelist --stdin $R1_path --bc-pattern=$protocol --set-cell-number=$sample_size --log2stderr > $wd/whitelist.txt`;
-say "done";
-
-say "Extract barcdoes and UMIs and add to read names";
-$umi = `/home/ubuntu/anaconda3/bin/umi_tools extract --bc-pattern=$protocol --stdin $R1_path --stdout $wd/R1_extracted.fastq.gz --read2-in $R2_path --read2-out=$wd/R2_extracted.clean.fastq.gz --filter-cell-barcode --whitelist $wd/whitelist.txt`;
-say "done";
-
+    say "Identify correct cell barcodes";
+    $umi = `/home/ubuntu/anaconda3/bin/umi_tools whitelist --stdin $R1_path --bc-pattern=$protocol --set-cell-number=$sample_size --log2stderr > $wd/whitelist.txt`;
+    say "done";
+    say "Extract barcdoes and UMIs and add to read names";
+    $umi = `/home/ubuntu/anaconda3/bin/umi_tools extract --bc-pattern=$protocol --stdin $R1_path --stdout $wd/R1_extracted.fastq.gz --read2-in $R2_path --read2-out=$wd/R2_extracted.clean.fastq.gz --filter-cell-barcode --whitelist $wd/whitelist.txt`;
+    say "done";
 }
 
 
@@ -134,37 +120,31 @@ say "Aligning fastq";
 my $aln ="";
 
 if($species eq "M" || $species eq "m"){
-
-if($aligner eq "STAR"){
-$aln = `/usr/bin/STAR --runThreadN 6 --genomeDir /home/ubuntu/Databases/GRCm38/STAR/ --readFilesIn $wd/$R2_name.clean.fastq.gz --readFilesCommand zcat --outSAMattributes Standard --outFileNamePrefix $wd/$R2_name`;
-$species = "GRCm38";
+    if($aligner eq "STAR"){
+        $aln = `/usr/bin/STAR --runThreadN 6 --genomeDir /home/ubuntu/Databases/GRCm38/STAR/ --readFilesIn $wd/$R2_name.clean.fastq.gz --readFilesCommand zcat --outSAMattributes Standard --outFileNamePrefix $wd/$R2_name`;
+        $species = "GRCm38";
 }
-
-
-elsif($aligner eq "hisat2"){
-$aln = `/home/ubuntu/bunga_tools/hisat2-2.1.0/hisat2 --phred33 -p 2 -x /home/ubuntu/Databases/GRCm38/hisat_index/GRCm38.primary_assembly.genome.fa.hisat2 -U $wd/$R2_name.clean.fastq.gz | awk '\$5 >= 60' > $wd/$R2_name.sam`;
-    	$species = "GRCm38";
-}
-
-}
-
-elsif($species eq "H" || $species eq "h"){
-
-if($aligner eq "STAR"){
-           $aln = `/usr/bin/STAR --runThreadN 6 --genomeDir /home/ubuntu/Databases/GRCh38/STAR/ --readFilesIn $wd/$R2_name.clean.fastq.gz --readFilesCommand zcat --outSAMattributes Standard --outFileNamePrefix $wd/$R2_name`;
-	$species = "GRCh38";        
-}
-
 
     elsif($aligner eq "hisat2"){
-            $aln = `/home/ubuntu/bunga_tools/hisat2-2.1.0/hisat2 --phred33 -p 2 -x /home/ubuntu/Databases/GRCh38/hisat_index/GRCh38.primary_assembly.genome.fa.hisat2 -U $wd/$R2_name.clean.fastq.gz | awk '\$5 >= 60' > $wd/$R2_name.sam`;
-        $species = "GRCh38";
-
+        $aln = `/home/ubuntu/bunga_tools/hisat2-2.1.0/hisat2 --phred33 -p 2 -x /home/ubuntu/Databases/GRCm38/hisat_index/GRCm38.primary_assembly.genome.fa.hisat2 -U $wd/$R2_name.clean.fastq.gz | awk '\$5 >= 60' > $wd/$R2_name.sam`;
+        $species = "GRCm38";
     }
 
 }
 
+elsif($species eq "H" || $species eq "h"){
+    if($aligner eq "STAR"){
+	    $aln = `/usr/bin/STAR --runThreadN 6 --genomeDir /home/ubuntu/Databases/GRCh38/STAR/ --readFilesIn $wd/$R2_name.clean.fastq.gz --readFilesCommand zcat --outSAMattributes Standard --outFileNamePrefix $wd/$R2_name`;
+	    $species = "GRCh38";        
+    }
 
+    elsif($aligner eq "hisat2"){
+		$aln = `/home/ubuntu/bunga_tools/hisat2-2.1.0/hisat2 --phred33 -p 2 -x /home/ubuntu/Databases/GRCh38/hisat_index/GRCh38.primary_assembly.genome.fa.hisat2 -U $wd/$R2_name.clean.fastq.gz | awk '\$5 >= 60' > $wd/$R2_name.sam`;
+    	$species = "GRCh38";
+
+    }
+
+}
 say "Alignment done";
 
 
@@ -174,16 +154,17 @@ say "Alignment done";
 #------------------------------------ POST-PROCESSING ------------------------------------
 #Convert to BAM
 say "converting to BAM";
-
 my $star = "R2_extractedAligned.out.sam"; 
+
 if($aligner eq "hisat2"){
-$exe = `/home/ubuntu/anaconda3/bin//samtools view -@ 2 -T /home/ubuntu/Databases/$species/genome/$species.primary_assembly.genome.fa -bS $wd/$R2_name.sam > $wd/$R2_name.bam`;
-say "Done";
+    $exe = `/home/ubuntu/anaconda3/bin//samtools view -@ 2 -T /home/ubuntu/Databases/$species/genome/$species.primary_assembly.genome.fa -bS $wd/$R2_name.sam > $wd/$R2_name.bam`;
+	say "Done";
 }
 elsif($aligner eq "STAR"){
-$exe = `/home/ubuntu/anaconda3/bin//samtools view -@ 2 -T /home/ubuntu/Databases/$species/genome/$species.primary_assembly.genome.fa -bS $wd/$star > $wd/$R2_name.bam`;
-say "Done";
+	$exe = `/home/ubuntu/anaconda3/bin//samtools view -@ 2 -T /home/ubuntu/Databases/$species/genome/$species.primary_assembly.genome.fa -bS $wd/$star > $wd/$R2_name.bam`;
+	say "Done";
 }
+
 #Sort the BAM
 say "\nSorting BAM";
 $exe = `/home/ubuntu/anaconda3/bin//samtools sort -T .  -m 2G  -@ 2 $wd/$R2_name.bam > $wd/$R2_name.sorted.bam`;
@@ -197,11 +178,12 @@ say "Done";
 #Add gene tag
 say "\nadding gene tag";
 my $fc = "/home/ubuntu/bunga_tools/subread-1.6.4-source/bin/./featureCounts";
+
 if($species eq "GRCm38"){
-$exe = `$fc -R BAM  --tmpDir . -T 2  -F GTF  -a /home/ubuntu/Databases/$species/annotations/gencode.vM21.annotation.meta_genes.gtf -o $wd/$R2_name.sorted.bam.featureCounts $wd/$R2_name.sorted.bam`;
+	$exe = `$fc -R BAM  --tmpDir . -T 2  -F GTF  -a /home/ubuntu/Databases/$species/annotations/gencode.vM21.annotation.meta_genes.gtf -o $wd/$R2_name.sorted.bam.featureCounts $wd/$R2_name.sorted.bam`;
 }
 elsif($species eq "GRCh38"){
-$exe = `$fc -R BAM  --tmpDir . -T 2  -F GTF  -a /home/ubuntu/Databases/$species/annotations/gencode.v31.annotation.meta_genes.gtf -o $wd/$R2_name.sorted.bam.featureCounts $wd/$R2_name.sorted.bam`;
+	$exe = `$fc -R BAM  --tmpDir . -T 2  -F GTF  -a /home/ubuntu/Databases/$species/annotations/gencode.v31.annotation.meta_genes.gtf -o $wd/$R2_name.sorted.bam.featureCounts $wd/$R2_name.sorted.bam`;
 }
 say "Done";
 
@@ -222,44 +204,39 @@ say "extracting the counts matrix";
 `/home/ubuntu/miniconda3/bin/R --no-save --no-restore --slave --args $wd/counts.tsv $wd/$id.markers.csv < /home/ubuntu/pre_process/remove_geneid.R`;
 
 if($pp eq "no"){
-#gzip the file
-`/bin/gzip $wd/$id.markers.csv`;
-
-#move the markers file
-`mv $wd/$id.markers.csv.gz /var/www/html/scw_page/scw_out/`;
-my $new_file = "https://www.bhasinlab.us/scw_page/scw_out/".$id.".markers.csv.gz";
-
-#Send email to the user
-`/home/ubuntu/anaconda3/bin//python3 /home/ubuntu/pre_process/smtp_ssl.py -to $to_email -linkout $new_file`;
-`rm -rfv $wd`;
+	#gzip the file
+	`/bin/gzip $wd/$id.markers.csv`;
+	#move the markers file
+	`mv $wd/$id.markers.csv.gz /var/www/html/scw_page/scw_out/`;
+	my $new_file = "https://www.bhasinlab.us/scw_page/scw_out/".$id.".markers.csv.gz";
+	#Send email to the user
+	`/home/ubuntu/anaconda3/bin//python3 /home/ubuntu/pre_process/smtp_ssl.py -to $to_email -linkout $new_file`;
+	`rm -rfv $wd`;
 }
 
 elsif($pp eq "yes"){
-my $csv = $wd.$id.".markers.csv";
-`mv $csv /home/ubuntu/project/temp/`;
+	my $csv = $wd.$id.".markers.csv";
+	`mv $csv /home/ubuntu/project/temp/`;
+	`rm -rf $wd*`;
+	`mv /home/ubuntu/project/temp/* $wd`;
+	`mkdir $wd/post_process_out`;
 
+	#post processing
+	`/home/ubuntu/miniconda3/bin//python3 /home/ubuntu/pre_process/post_process/scanPy.py -i $wd/$id.markers.csv -o $wd/post_process_out/ -sp $sp -mg $min_genes -mc $min_cells`;
 
-`rm -rf $wd*`;
-`mv /home/ubuntu/project/temp/* $wd`;
-`mkdir $wd/post_process_out`;
+	#gzip the markers file
+	`/bin/gzip $wd/$id.markers.csv`;
 
-#post processing
-`/home/ubuntu/miniconda3/bin//python3 /home/ubuntu/pre_process/post_process/scanPy.py -i $wd/$id.markers.csv -o $wd/post_process_out/ -sp $sp -mg $min_genes -mc $min_cells`;
+	#gzip the working directory
+	`cd $wd && /bin/tar -czf $id.out.tar.gz *`;
+	`mv $wd/$id.out.tar.gz /var/www/html/scw_page/scw_out/`;
 
-#gzip the markers file
-`/bin/gzip $wd/$id.markers.csv`;
+	#Send email to the user
+	my $new_file = "https://www.bhasinlab.us/scw_page/scw_out/".$id.".out.tar.gz";
+	`/home/ubuntu/anaconda3/bin//python3 /home/ubuntu/pre_process/smtp_ssl.py -to $to_email -linkout $new_file`;
 
-#gzip the working directory
-`cd $wd && /bin/tar -czf $id.out.tar.gz *`;
-`mv $wd/$id.out.tar.gz /var/www/html/scw_page/scw_out/`;
-
-#Send email to the user
-my $new_file = "https://www.bhasinlab.us/scw_page/scw_out/".$id.".out.tar.gz";
-`/home/ubuntu/anaconda3/bin//python3 /home/ubuntu/pre_process/smtp_ssl.py -to $to_email -linkout $new_file`;
-
-
-#Delete the files
-`rm -rfv $wd`;
+	#Delete the files
+	`rm -rfv $wd`;
 }
 
 #------------------------------------ END OF PROGRAM ------------------------------------
